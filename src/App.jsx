242399b70d6cdcb9c810110abe7ddc19d5d97331
João <joaobrit0000000000@gmail.com>
2023-07-17 20:33:07 +0100

    Code Rock Paper Scissors

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,99 @@
 import { useState } from 'react'
-
 import './App.css'
-
+//I will try to make a rock paper scissors game
+// In react!!! good luck for me!!
 function App(){
-  const [squares, setSquare] = useState(Array(9).fill(null));
-
-
-  function handleClick(){
-    console.log("hello prettey")
+  let   [points,setPoints] = useState(0);
+  let [pc_points,setPcPoints] = useState(0);
+  let [color, setColor] = useState("white");
+  let [pc_hand, setPcHand] = useState("");
+  const options = [0,1,2];
+  const emoji_options = ["✋","✌️","✊"];
+  const cases = [
+    [0,1,-1],
+    [-1,0,1],
+    [1,-1,0]
+  ]
+  function CompareComputer(name){
+    let computerGuess= options[Math.floor(Math.random()*options.length)];
+    if(!(cases[computerGuess][name]==-1)){
+      setPoints(points+ cases[computerGuess][name])
+      setPcHand(emoji_options[computerGuess])      
+      if(cases[computerGuess][name]==0){
+        setColor("white");
+      }else{
+        setColor("green")
+      }
+    }else{
+      setColor("red")
+      setPcPoints(pc_points+1)
+    }
+  }
+  function resetPoints(){
+    setPoints(0);
+    setPcPoints(0);
+    setColor("white");
   }
   return(
-    <div>
-      <Heading/>
-      <div>
-        <Square value={squares[0]} onSquareClick={handleClick}/>
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
-      </div>
-      <div>
-        <Square value={squares[3]} onSquareClick={handleClick}/>
-        <Square value={squares[4]} onSquareClick={handleClick}/>
-        <Square value={squares[5]} onSquareClick={handleClick}/>
-      </div>
-
-      <div>
-        <Square value={squares[6]} onSquareClick={handleClick}/>
-        <Square value={squares[7]} onSquareClick={handleClick}/>
-        <Square value={squares[8]} onSquareClick={handleClick}/>
-      </div>
-      
+    <div style={{
+      backgroundColor:color,
+      width:"40em",
+      height:"40em",
+      display:"flex",
+      flexDirection:"column",
+      alignItems:"center",
+      justifyContent:"center"
+    }}>
+    <Points point={points} pcp={pc_points}/>
+    <PcHand choice={pc_hand}/>
+    <div >
+      <Options name={"✋"} handleClick={()=> CompareComputer(0)}/>
+      <Options name={"✌️"} handleClick={()=>CompareComputer(1)}/>
+      <Options name={"✊"} handleClick={()=>CompareComputer(2)}/>
+    </div>
+    <Reset handleClick={resetPoints}/>
     </div>
   )
 }
-export function Square({value, onSquareClick}){
-  
+export function Options({name,handleClick}){
+
   return(
-    <button className='square' onClick={onSquareClick}> {value} </button>
+    <
+      button
+      style={{
+        fontSize:"1.3em",
+        marginInline:"0.3em",
+        backgroundColor:"RGBA(0,0,0,0.5)"
+      }}
+      onClick={handleClick}
+    >
+    {name}
+    </button>
   )
 }
-
-export function Heading(){
+export function Points({point, pcp}){
   return(
-    <>
-      <h1>Making a tic tac toe</h1>
-
-    </>
+    <div
+    style={{
+      color:"black",
+      display:"flex",
+      fontSize:"0.5em",
+      gap:"30em",
+      alignItems:"center"
+    }}>
+      <h1>Points: {point}</h1>
+      <h1 style={{alignSelf:"flex-end"}}>Computer's points: {pcp}</h1>
+    </div>
   )
 }
-
+export function PcHand({choice}){
+  return(
+    <p style={{fontSize:"3em"}}>{choice}</p>
+  )
+}
+export function Reset({handleClick}){
+  return(
+    <button onClick={handleClick}>↻</button>
+  )
+}
 export default App

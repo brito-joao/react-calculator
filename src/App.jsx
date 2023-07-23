0c75ca857d6c74b4022a478bdf0010343ccb3da8
João <joaobrit0000000000@gmail.com>
2023-07-23 18:10:15 +0100

    Add addition and subtraction with multiple number support

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,23 @@ function App(){
     if(ended){
       setExpression(`${value}`);
       setEnded(false);
-      
     }else{
       let new_var= `${expression}${value}`
       setExpression(new_var)
     }
-    
-    
   }
-
   function pressedOperator(value){
     const options = ["/","x","-","+"]
     console.log(value)
     //seeing if the button pressed was the "="
     if(!options.includes(value)){
-      
       setExpression(solver(expression))
       setEnded(true);
-      
     }else{
       setExpression(`${expression}${value}`);
     }
   }
   useEffect(()=>{
-    
   },[expression])
   return(
     <div className='buttons'>
@@ -57,88 +50,98 @@ function App(){
       <button></button>
       <button>,</button>
       <Operator handleClick={()=>{pressedOperator("=")}} operator={"="}/>
-      
     </div>
   )
-
 }
 export function Display({toDisplay}){
   return(
-    <div>{toDisplay}</div>
+    <div style={{color:"white"}}>{toDisplay}</div>
   )
 }
-
 export function Operator({operator,handleClick}){
   return(
     <button className='operator' onClick={handleClick}>{operator}</button>
   )
 }
-
 export function Number({number,handleClick}){
   return(
-    <button onClick={handleClick}>{number}</button>
+    <button style={{backgroundColor:"black"}} onClick={handleClick}>{number}</button>
   )
 }
-
+//gets an operation and solves it in the correct order with 2 or more numbers
 function calculateResultSimple(array,operators){
-  
   let operator_quantities = [0,0,0,0];
-  
   array.forEach((item)=>{
     if(operators.includes(item)){
       operator_quantities[operators.indexOf(item)]+=1;
     }
   })
-  //find the order of operations
-  console.log(operator_quantities,"hello people");
-
-  if(operator_quantities[0] || operator_quantities[1]){
-    console.log("hello wolrdo",array);
-  }
   let temporary_result;
   //first for the "x" and "/"'s
   for(var i=0; i<(operator_quantities[0]+operator_quantities[1]);i++){
     array.forEach((item,index)=>{
       if(item=="x" || item=="/"){
-      //if operator is not the last item, nor the first
-      //if side items arent other operators
-      //if side items are "-" 
-      
       if(array.at(-1)==item){
-        console.log("you dumb idiot, you can't do that",item);
         array.splice(array.length-1,1);
       }else{
         if(item=="x"){
           temporary_result = parseFloat(array[index-1])*parseFloat(array[index+1])
-          
         }else{
           temporary_result = (parseFloat(array[index-1])/parseFloat(array[index+1])).toFixed(2)
         }
-        console.log("part1",array);
-        console.log("tempa sda", temporary_result)
         array[index]=temporary_result;
         array.splice(index-1,1);
         array.splice(index,1);
-        
-        console.log("part2",array);
-        
       }
-      console.log("hello asdlfajs",array);
     }
     })
   }
-    
-  
-  //find if the operator is in the middle
+  for(var i=0;i<operator_quantities[2]+operator_quantities[3];i++){
+    array.forEach((item,index)=>{
+      if(item=="+" || item=="-"){
+        if(array.at(-1)==item){  
+          array.splice(array.length-1,1);
+        }else{
+          if(item=="+"){
+            temporary_result = parseFloat(array[index-1])+parseFloat(array[index+1])   
+          }else{
+            temporary_result = (parseFloat(array[index-1])-parseFloat(array[index+1])).toFixed(2)
+          }     
+          array[index]=temporary_result;
+          array.splice(index-1,1);
+          array.splice(index,1);
+        }
+    }
+    })
+  }
 
-  //calculate result with 2d array
-
-  
+  console.log(array)
   return array[0];
 }
-
+//to prepare the array for the next function 
+function mergeNumbers(array,operators){
+  let number= "";
+  let operator = "";
+  let operation = [];
+  array.forEach((item,index)=>{
+    if(operators.includes(item)){
+      console.log("hello world");
+      operation.push(number);
+      operation.push(item);
+      number="";
+    }else{
+      number+=item;
+    }
+  });
+  if(number){
+    operation.push(number);
+  }
+  //create something that can split by the operators
+  ;
+  console.log(operation);
+  return operation
+}
 function solver(string){
-  
   let arrayed_expression = string.split("");
   const operators = ["/","x","-","+"];
   let has_operators = false;
@@ -147,13 +150,12 @@ function solver(string){
       console.log("found one!",item);
       has_operators=true;
     }
-    
   })
   if(has_operators){
-    return calculateResultSimple(arrayed_expression,operators);
+    //merge all the numbers pressed into one 
+    return calculateResultSimple(mergeNumbers(arrayed_expression,operators),operators);
   }else{
     return Math.floor(Math.random()*100)
   }
-  
 }
 export default App
